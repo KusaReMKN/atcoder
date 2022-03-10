a9053f088ea278cd06feec8f5909d5fd3c60666f
KusaReMKN <48670724+KusaReMKN@users.noreply.github.com>
2022-03-10 23:28:59 +0900

    add abc122 a

---
+++ abc/122/a.c
@@ -0,0 +1,17 @@
+main()
+{
+	switch (getchar()) {
+	case 'A':
+		puts("T");
+		break;
+	case 'C':
+		puts("G");
+		break;
+	case 'G':
+		puts("C");
+		break;
+	case 'T':
+		puts("A");
+		break;
+	}
+}
