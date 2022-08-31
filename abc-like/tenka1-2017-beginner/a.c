7289c0209b59a2f3da7cb74d3c6badca0090b3de
KusaReMKN <48670724+KusaReMKN@users.noreply.github.com>
2022-09-01 06:26:14 +0900

    add tenka1-2017-beginner a

---
+++ abc-like/tenka1-2017-beginner/a.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int
+main(void)
+{
+	int s1, s2, s3, s4, s5, s6;
+
+	scanf("%1d%1d%1d%1d%1d%1d", &s1, &s2, &s3, &s4, &s5, &s6);
+	printf("%d\n", s1 + s2 + s3 + s4 + s5 + s6);
+
+	return 0;
+}
