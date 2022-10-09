2affd0f10963c18467286d47feaa34f496693c50
KusaReMKN <48670724+KusaReMKN@users.noreply.github.com>
2022-10-09 16:58:57 +0900

    add abc272 a

---
+++ abc/272/a.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int
+main(void)
+{
+	int n, a;
+	int sum;
+
+	scanf("%d", &n);
+	sum = 0;
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &a);
+		sum += a;
+	}
+	printf("%d\n", sum);
+
+	return 0;
+}
