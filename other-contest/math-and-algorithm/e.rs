9f4dc0a17a434b6ca864319cb9b89d11e275761d
KusaReMKN <48670724+KusaReMKN@users.noreply.github.com>
2023-01-09 19:21:14 +0900

    add math-and-algorithm e
    https://atcoder.jp/contests/math-and-algorithm/submissions/37903326

---
+++ other-contest/math-and-algorithm/e.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let mut n = String::new();
+    std::io::stdin().read_line(&mut n).ok();
+    let n: i32 = n.trim().parse().unwrap();
+
+    let mut a = String::new();
+    std::io::stdin().read_line(&mut a).ok();
+    let a: Vec<i32> = a.split_whitespace()
+        .map(|e| e.parse().unwrap()).collect();
+
+    let sum: i32 = a.iter().sum();
+    println!("{}", sum % 100);
+}
