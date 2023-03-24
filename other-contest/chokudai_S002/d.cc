bc6de57b3e0096c66ffa23cc8c65b1f774014989
KusaReMKN <48670724+KusaReMKN@users.noreply.github.com>
2023-03-25 00:22:33 +0900

    add chokudai_S002 d
    https://atcoder.jp/contests/chokudai_S002/submissions/39995628

---
+++ other-contest/chokudai_S002/d.cc
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <iostream>
+
+int
+main(void)
+{
+	int n;
+	std::cin >> n;
+
+	long sum = 0;
+	for (int i = 0; i < n; i++) {
+		int a, b;
+		std::cin >> a >> b;
+
+		sum += std::max(a, b);
+	}
+	std::cout << sum << std::endl;
+
+	return 0;
+}
