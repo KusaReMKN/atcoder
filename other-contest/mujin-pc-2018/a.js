02d2f08d0bfdc88c450af6f46e9f1c69ca81c411
KusaReMKN <48670724+KusaReMKN@users.noreply.github.com>
2023-06-20 21:54:42 +0900

    add mujin-pc-2018 a
    https://atcoder.jp/contests/mujin-pc-2018/submissions/42768369

---
+++ other-contest/mujin-pc-2018/a.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('/dev/stdin', 'utf-8');
+const lines = input.split('\n');
+const s = lines.shift();
+
+console.log(/^MUJIN/.test(s) ? 'Yes' : 'No');
