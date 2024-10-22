51ae1bcb7c0146298b1ab452b79e7102ee04b474
KusaReMKN <48670724+KusaReMKN@users.noreply.github.com>
2024-10-22 09:16:06 +0900

    update abc240 d
    https://atcoder.jp/contests/abc240/submissions/59047491

--- abc/240/d.cc
+++ abc/240/d.cc
@@ -8,22 +8,23 @@ main(void)
 	int n;
 	std::cin >> n;
 
+	int cur = 0;
 	std::vector<std::pair<int, int>> v;
 	v.push_back({ -1, 0 });
 	for (int i = 0; i < n; i++) {
 		int a;
 		std::cin >> a;
+		cur++;
 		if (v.back().first == a)
 			v.back().second++;
 		else
 			v.push_back({ a, 1 });
-		if (v.back().first == v.back().second)
-			v.pop_back();
 
-		int cnt = 0;
-		for (const auto &e: v)
-			cnt += e.second;
-		std::cout << cnt << std::endl;
+		if (v.back().first == v.back().second) {
+			cur -= v.back().second;
+			v.pop_back();
+		}
+		std::cout << cur << std::endl;
 	}
 
 	return 0;
